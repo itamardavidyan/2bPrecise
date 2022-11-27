0bbff72395c37d6f26cf8c0cf07841da91ae8156
Itamar Davidyan <itamardavidyan@gmail.com>
2022-11-27 14:12:55 +0200

    Delete app controller and service

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 import { EmployeesModule } from './employees/employees.module';
 import { ManagersModule } from './managers/managers.module';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [EmployeesModule, ManagersModule, TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EmployeesModule, ManagersModule, TasksModule]
 })
 export class AppModule {}
